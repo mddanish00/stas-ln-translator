786570bdb5231c61963f90d0773efce518b66421
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2025-07-10 23:14:15 +0800

    feat: implement initial CLI for stas-ln-translator
    feat: implement config for whole app usage

--- src/stas_ln_translator/__init__.py
+++ src/stas_ln_translator/__init__.py
@@ -1,2 +1,95 @@
-def main() -> None:
-    print("Hello from stas-ln-translator!")
+import sys
+import importlib.metadata
+import click
+
+
+from stas_ln_translator import config
+
+
+# Call back for validating file need to have '.epub' file extension
+def validate_epub_file(ctx, param, value):
+    if not value.endswith(".epub"):
+        raise click.BadParameter(
+            f"Input file {value} must have a .epub extension"
+            if param.name == "input"
+            else f"Output file {value} must have a .epub extension"
+        )
+    return value
+
+
+def cleanup_temp_dir():
+    if config.temp_dir is not None:
+        import shutil
+
+        shutil.rmtree(config.temp_dir)
+        click.echo(f"Temporary directory '{config.temp_dir}' cleaned up")
+
+
+@click.command(
+    context_settings=dict(help_option_names=["-h", "--help"]),
+    help="Run stas-ln-translator, an alternative standalone Light Novel translator for Sugoi Translator. Recommended usage with stas-server.",
+)
+@click.version_option(None, "-v", "--version", message="%(version)s")
+@click.option(
+    "--batch_size",
+    required=False,
+    default=100,
+    help="Number of lines per batch in a request. Only works when request_type is 'Batch'.",
+)
+@click.option(
+    "--request_type",
+    required=False,
+    default="Batch",
+    help="Request type used when contacting API. Either 'Single' or 'Batch'(default).",
+)
+@click.option(
+    "--tag_to_translate",
+    required=False,
+    help="HTML elements to translate, separated by commas",
+    default="p,h1,h2,h3,h4,h5,h6,title",
+)
+@click.option(
+    "--translator_api_url",
+    required=False,
+    help="stas-server or any other Sugoi Translator compatible API URL",
+    default="http://localhost:14366",
+)
+@click.argument(
+    "input",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True),
+    # help="Input EPUB file",
+    callback=validate_epub_file,
+)
+@click.argument(
+    "output",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True),
+    # help="Output EPUB file",
+    callback=validate_epub_file,
+)
+def cli(
+    input,
+    output,
+    translator_api_url,
+    request_type,
+    batch_size,
+    tag_to_translate,
+):
+    try:
+        config.loads(
+            input,
+            output,
+            batch_size,
+            request_type,
+            tag_to_translate,
+            translator_api_url,
+        )
+        click.echo(
+            f"stas-ln-translator - v{importlib.metadata.version('stas-ln-translator')}"
+        )
+        config.print_config(click.echo)
+        # Clean up temporary directory
+        cleanup_temp_dir()
+    except KeyboardInterrupt:
+        click.echo("\nCtrl+C detected. Performing graceful shutdown...")
+        cleanup_temp_dir()
+        sys.exit(0)
---
+++ src/stas_ln_translator/config.py
@@ -0,0 +1,69 @@
+# Using The Global Object Pattern described on
+# https://python-patterns.guide/python/module-globals/.
+
+
+from typing import Callable
+from tempfile import mkdtemp
+
+# How many line per batch request
+batch_size = 100
+# If request type is `Single`, batch_size of 1 will be enforced
+# Also, post request from translation API with "translate sentence" request.
+# If request type is `Batch`, post request from translation API with "translate batch" request.
+request_type = "Batch"
+# HTML elements to translate
+tag_to_translate = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "title"]
+# Translator API URL
+translator_api_url = "http://localhost:14366"
+# Input EPUB file
+input = "input.epub"
+# Output EPUB file
+output = "output.epub"
+# Temporary directory
+temp_dir = None
+
+
+def loads(
+    loaded_input: str,
+    loaded_output: str,
+    loaded_batch_size: int,
+    loaded_request_type: str,
+    loaded_tag_to_translate: str,
+    loaded_translator_api_url: str,
+    loaded_temp_dir: str = None,
+):
+    global \
+        input, \
+        output, \
+        batch_size, \
+        request_type, \
+        tag_to_translate, \
+        translator_api_url, \
+        temp_dir
+
+    input = loaded_input
+    output = loaded_output
+    batch_size = loaded_batch_size
+    request_type = loaded_request_type
+    tag_to_translate = loaded_tag_to_translate.split(",")
+    translator_api_url = loaded_translator_api_url
+    if loaded_temp_dir is None:
+        temp_dir = mkdtemp(prefix="stas-ln-translator-")
+    else:
+        temp_dir = loaded_temp_dir
+
+
+# Print current config values
+# Can replace print function
+# By default, use normal print function, can be changed to click.echo
+def print_config(print_func: Callable[[str], None] = print):
+    print_func("-------------------------------------------------------")
+    print_func("Current configuration:")
+    print_func(f"-- Input EPUB file: {input}")
+    print_func(f"-- Output EPUB file: {output}")
+    print_func(f"-- Batch size: {batch_size}")
+    print_func(f"-- Request type: {request_type}")
+    print_func(f"-- HTML elements to translate: {tag_to_translate}")
+    print_func(f"-- Translator API URL: {translator_api_url}")
+    print_func(f"-- Temporary directory: {temp_dir}")
+    print_func("-------------------------------------------------------")
