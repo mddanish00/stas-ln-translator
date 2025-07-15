4752ae1b30e516cb9feac8126edb0caeb8fe71c4
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2025-07-15 12:57:44 +0800

    feat: add translation request generator

---
+++ src/stas_ln_translator/connection.py
@@ -0,0 +1,18 @@
+import httpx
+
+
+async def create_translation_request(
+    texts: list[str], client: httpx.AsyncClient, api_url: str, type: str
+) -> list[str]:
+    json_data = (
+        {"batch": texts, "message": "translate batch"}
+        if type == "Batch"
+        else {
+            "content": texts[0],
+            "message": "translate sentence",
+        }
+    )
+
+    response = await client.post(api_url, json=json_data)
+
+    return list(response.json()) if type == "Batch" else [response.json()]
